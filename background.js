45cf5f331a937d08852858c1705757176370f1ca
preston.nowakowski <preston.nowakowski@servicenow.com>
2019-09-06 12:53:50 -0700

    Create js script that runs on dom load event

--- background.js
+++ background.js
@@ -4,11 +4,17 @@ chrome.runtime.onInstalled.addListener(function() {
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostContains: '.service-now.com'},
       })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+    ],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});  
 });
+
+// chrome.tabs.executeScript(undefined, {
+//   code: 'console.log("this script is working")'
+// })
+
+
 
 chrome.commands.onCommand.addListener(function(command) {
   console.log('Command:', command);
