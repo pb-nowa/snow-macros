fe1b35f379b7740330e91a47ba6109201cbc04b9
preston.nowakowski <preston.nowakowski@servicenow.com>
2019-10-07 10:41:34 -0700

    Create message listener flow from contentScript to background script

--- background.js
+++ background.js
@@ -4,18 +4,55 @@ chrome.runtime.onInstalled.addListener(function() {
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostContains: '.service-now.com'},
       })
-    ],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-  }]);
-});  
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });  
 });
 
-// chrome.tabs.executeScript(undefined, {
-//   code: 'console.log("this script is working")'
-// })
+function redirect(endUrl) {
+  chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+      const tab = tabs[0]
+      const urlChunk = tab.url.split(/uri=/)[0];
 
+      const url = urlChunk + endUrl
+      chrome.tabs.update(tab.id, { url });
+  })
 
+}
+
+/*
+old commands through chrome api
 
 chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
+  switch (command) {
+    case "redirect_to_sys_user":
+      redirect("uri=%2Fsys_user_list.do");
+      break;
+    case "redirect_to_sys_user_group":
+      redirect("uri=%2Fsys_user_group_list.do");
+      break;
+    case "redirect_to_tables":
+      redirect("uri=%2Fsys_db_object_list.do");
+      break;
+    default:
+      break;
+  }
 });
+*/
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.action) {
+    case "redirect_to_sys_user":
+      redirect("uri=%2Fsys_user_list.do");
+      break;
+    case "redirect_to_sys_user_group":
+      redirect("uri=%2Fsys_user_group_list.do");
+      break;
+    case "redirect_to_tables":
+        redirect("uri=%2Fsys_db_object_list.do");
+        break;
+    default:
+      break;
+  }
+})
--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,57 @@
-console.log("*******CONTENT SCRIPT IS RUN********")
+if (window.location.origin.includes("service-now")) {
+    console.log("*******CONTENT SCRIPT IS RUN********")
+    console.log("DocumentState: " + document.readyState);
+    
+    function init() {
+        const filterNav = document.getElementById("editing-view-port");
+        const newBtn = document.getElementById("sysverb_new");
+        const mainWindow = document.getElementById("gsft_main")
+        console.log(mainWindow);
+    }
+    
+    window.onload = () => {
+        console.log("DocumentState: " + document.readyState);
+        init();
+    }
+
+    document.addEventListener('keydown', e => {
+        if (e.ctrlKey && e.shiftKey) {
+            switch (e.keyCode) {
+                case 85:
+                    chrome.runtime.sendMessage({action: "redirect_to_sys_user", key: "U"}, res => {
+                        console.log("redirect_to_sys_user");
+                    })
+                    break;
+                default:
+                case 71:
+                    chrome.runtime.sendMessage({action: "redirect_to_sys_user_group", key: "G"}, res => {
+                        console.log("redirect_to_sys_user_group");
+                    })
+                    break;
+                case 84:
+                    chrome.runtime.sendMessage({action: "redirect_to_tables", key: "T"}, res => {
+                        console.log("redirect_to_tables");
+                    })
+                    break;
+                default:
+                    break;
+            }
+        }
+        console.log(e.keyCode);
+    });
+    
+
+
+    //ctrlKey
+    //shiftKey
+}
+
+
+
+//cmd: 91
+//opt: 18
+//ctl: 17
+//shift: 16
+
+//C: 67
+
