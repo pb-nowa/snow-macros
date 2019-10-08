605661cb7f4f1553ae672258b4126eddc30364e7
preston.nowakowski <preston.nowakowski@servicenow.com>
2019-10-07 17:38:48 -0700

    Try to override gsft_main script override, in progress

--- shadowBreaker.js
+++ shadowBreaker.js
@@ -2,3 +2,7 @@ Element.prototype._attachShadow = Element.prototype.attachShadow;
 Element.prototype.attachShadow = function() {
     return this._attachShadow({ mode: "open" });
 };
+
+if (window.name == "gsft_main"){
+    window.name = "gsft_main"
+}
