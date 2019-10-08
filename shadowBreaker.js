94e1f44e6f2afeea2f173ede536f31d0ae96ac6c
preston.nowakowski <preston.nowakowski@servicenow.com>
2019-10-08 12:16:55 -0700

    Comment out attachShadow override since it was destroying '/workspace'

--- shadowBreaker.js
+++ shadowBreaker.js
@@ -1,9 +1,5 @@
-if (window.name == "gsft_main"){
-    window.name = "gsft_main"
-}
-
-Element.prototype._attachShadow = Element.prototype.attachShadow;
-Element.prototype.attachShadow = function(params) {
-    params.mode = 'open';
-    return Element.prototype._attachShadow(params);
-}
+// Element.prototype._attachShadow = Element.prototype.attachShadow;
+// Element.prototype.attachShadow = function(params = {}) {
+//     params['mode'] = 'open';
+//     return Element.prototype._attachShadow(params);
+// }
