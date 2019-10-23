d3173401351729f1aa2ef19827e6d48e0b38db8d
Preston Nowakowski <pb.nowa@gmail.com>
2019-10-23 09:49:33 -0700

    Input now removes nodes from list, TODO: invoke adding nodes when deleting chars

--- popup.js
+++ popup.js
@@ -6,30 +6,37 @@ const keyContainer = document.getElementById("keybindings-container");
 
 const MACROS = {
     do_new: {  
+        id: "do_new",
         value: "Create New:",
         command: "cntl+shift+N"
     },
     do_save: {
+        id: "do_save",
         value: "Save:",
         command: "cntl+shift+S"
     },
     dir_groups: {
+        id: "dir_groups",
         value: "Redirect to Groups Table:",
         command: "cntl+shift+G"
     },
     dir_tables: {
+        id: "dir_tables",
         value: "Redirect to sys_tables Table:",
         command: "cntl+shift+T"
     },
     dir_users: {
+        id: "dir_users",
         value: "Rediret to Users Table:",
         command: "cntl+shift+U"
     },
     open_workspace: {
+        id: "open_workspace",
         value: "Open Workspace:",
         command: "cntl+shift+W"
     },
     open_studio: {
+        id: "open_studio",
         value: "Open Studio(Editor):",
         command: "cntl+shift+E"
     }
@@ -46,20 +53,22 @@ state.previous = [];
 
 function populate(list, prevState) {
     for (let item of list) {
+        const { id, value, command } = item;
+
         const listElement = document.createElement("LI");
         
         const name = document.createElement("DIV");
         name.className = "keybinding-label";
-        name.textContent = item.value;
+        name.textContent = value;
 
-        const command = document.createElement("DIV");
-        command.className = "keybinding";
-        command.textContent = item.command;
+        const keyBinding = document.createElement("DIV");
+        keyBinding.className = "keybinding";
+        keyBinding.textContent = command;
         
-        
+        listElement.id = id;
         listElement.className = "keybinding-item";
         listElement.appendChild(name)
-        listElement.appendChild(command)
+        listElement.appendChild(keyBinding)
         
         keyContainer.appendChild(listElement)
     }
@@ -67,20 +76,45 @@ function populate(list, prevState) {
 
 populate(state.current, state.previous)
 
-search.addEventListener('input', e => {
-    handleSearch(e.target.value)
-})
+function depopulate(list) {
+    const ids = list.map(macro => macro.id);
 
-function handleSearch(str) {
-    const macros = Object.values(MACROS);
-    console.log(macros.map(el => el.value))
-    
+    for (let listElement of keyContainer.children) {
+        if (ids.includes(listElement.id)) {
+            keyContainer.removeChild(listElement);
+        }
+    }
+}
+
+function searchFilter(str) {
+    const macros = Object.values(MACROS);    
     
     let macroState = macros.filter(macro => {
-        return macro.value.toLowerCase().includes(str);
-    })
+        const isSubstring = macro.value.toLowerCase().includes(str);
+        return isSubstring;
+    });
     
-    populate(macroState)
+    return macroState;
 }
+
+function toBeDeleted(str) {
+    const macros = Object.values(MACROS);
+
+    let macroState = macros.filter(macro => {
+        const isNotSubstring = !macro.value.toLowerCase().includes(str);
+        return isNotSubstring;
+    });
+
+    return macroState;
+}
+
+search.addEventListener('input', e => {
+    state.previous = Array.from(state.current);
+    state.current = searchFilter(e.target.value);
+    const deleteList = toBeDeleted(e.target.value);
+
+    depopulate(deleteList);
+    // populate(state.current, state.previous)
+})
 
 //sender  #id, #tab, #url
